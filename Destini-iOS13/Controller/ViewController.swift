a599aa90416814ad88a23cac40e9c9103e97999a
Stavros <snikoletopoulos@hotmail.com>
2024-04-14 14:08:03 +0300

    Implement story telling

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,16 +1,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
 
-    @IBOutlet weak var storyLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
-    
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateStory()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(choice: sender.currentTitle!)
 
+        updateStory()
+    }
+
+    func updateStory() {
+        let story = storyBrain.getActiveStory()
+
+        storyLabel.text = story.title
+
+        choice1Button.setTitle(story.firstChoice, for: .normal)
+        choice2Button.setTitle(story.secondChoice, for: .normal)
+    }
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -1,1 +1,9 @@
 import Foundation
+
+struct Story {
+    let title: String
+    let firstChoice: String
+    let firstChoiceDestination: Int
+    let secondChoice: String
+    let secondChoiceDestination: Int
+}
